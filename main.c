b88db2fbbdb706294a85b8b2bf42f16492126e02
borzhex <borzhex@proton.me>
2026-01-29 14:27:31 +0100

    Create main.c

---
+++ main.c
@@ -0,0 +1,25 @@
+/* file: main.c
+ * Основная программа практической работы
+ * Тема: операции с матрицами
+ * Студент: Илья Варкиодин
+ */
+
+#include <stdio.h>
+#include "matrix.h"
+
+int main(void) {
+    Matrix A, B, C;
+
+    matrix_input(&A);
+    matrix_input(&B);
+
+    if (matrix_add(&A, &B, &C) == 0) {
+        matrix_print(&C);
+    }
+
+    if (matrix_multiply(&A, &B, &C) == 0) {
+        matrix_print(&C);
+    }
+
+    return 0;
+}
