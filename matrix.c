e3569af09a9e10f1b48a9047697e289a27920105
borzhex <borzhex@proton.me>
2026-01-29 14:27:59 +0100

    Create matrix.c

---
+++ matrix.c
@@ -0,0 +1,64 @@
+/* file: matrix.c
+ * Реализация операций с матрицами
+ * Тема: операции с матрицами
+ * Студент: Илья Варкиодин
+ */
+
+#include <stdio.h>
+#include "matrix.h"
+
+void matrix_input(Matrix *m) {
+    scanf("%d %d", &m->rows, &m->cols);
+
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            scanf("%d", &m->data[i][j]);
+        }
+    }
+}
+
+void matrix_print(const Matrix *m) {
+    for (int i = 0; i < m->rows; i++) {
+        for (int j = 0; j < m->cols; j++) {
+            printf("%d ", m->data[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int matrix_add(const Matrix *a, const Matrix *b, Matrix *result) {
+    if (a->rows != b->rows || a->cols != b->cols) {
+        return -1;
+    }
+
+    result->rows = a->rows;
+    result->cols = a->cols;
+
+    for (int i = 0; i < a->rows; i++) {
+        for (int j = 0; j < a->cols; j++) {
+            result->data[i][j] = a->data[i][j] + b->data[i][j];
+        }
+    }
+
+    return 0;
+}
+
+int matrix_multiply(const Matrix *a, const Matrix *b, Matrix *result) {
+    if (a->cols != b->rows) {
+        return -1;
+    }
+
+    result->rows = a->rows;
+    result->cols = b->cols;
+
+    for (int i = 0; i < result->rows; i++) {
+        for (int j = 0; j < result->cols; j++) {
+            result->data[i][j] = 0;
+            for (int k = 0; k < a->cols; k++) {
+                result->data[i][j] += a->data[i][k] * b->data[k][j];
+            }
+        }
+    }
+
+    return 0;
+}
